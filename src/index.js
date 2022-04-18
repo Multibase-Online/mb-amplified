64898f1aab526987829f7aebac63ea0c66eb1f79
Samena <s.rackley@hotmail.co.uk>
2022-04-18 17:30:21 +0100

    commented

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
+//adding random comment
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
